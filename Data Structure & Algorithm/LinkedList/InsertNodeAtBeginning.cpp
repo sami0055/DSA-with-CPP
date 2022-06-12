e9654d10788206eab8a35a8d6ffbdfaab0fc4267
Sami <83569514+sami0055@users.noreply.github.com>
2022-06-12 14:10:03 +0600

    LinkedList

---
+++ Data Structure & Algorithm/LinkedList/InsertNodeAtBeginning.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<stdio.h>
+#include<stdlib.h>
+using namespace std;
+struct Node{
+    int data;
+    struct Node* next;
+};
+
+struct Node* Head;
+
+void print()
+{
+    struct Node* temp;
+     temp=Head;
+     while(temp!=NULL)
+     {
+          cout<<temp->data<<endl;
+          temp=temp->next;
+     }
+}
+void insert(int value)
+{
+    
+   struct Node* temp=(struct Node*)malloc(sizeof(struct Node));
+   temp->data=value;
+   temp->next=Head;
+   Head=temp;
+   }
+  
+
+int main()
+{
+    Head=NULL;
+    insert(1);
+    insert(45);
+    insert(56);
+    insert(7);
+    print();
+}
